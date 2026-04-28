5ff39562a792245cb91752b8d1ef2d3d5f2a5bc7
Elsie Tavengwa <elsiet@Elsies-MacBook-Pro.local>
2026-04-28 19:59:18 +1000

    a

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const fs = require('fs');
 const app = express();
 const YOUR_DOMAIN = (process.env.YOUR_DOMAIN || 'http://localhost:3000').replace(/\/$/, '');
 // Serve your shop HTML as the frontend
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -31,7 +32,7 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(cors());
+
 
 // ============================================================
 // PRODUCT CATALOGUE
